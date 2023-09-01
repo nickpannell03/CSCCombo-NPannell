322b4beafcaaefff9d71ca7ca1d8846a1927be50
Pannell <S03012956@ccc.ccofc.edu>
2023-09-01 16:06:48 -0600

    first commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,26 @@
+/*********************************************
+ * This is a hello world project it writes hello world to the console
+ * written by nick pannell
+ * date 9/1/23
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		System.out.println();
+		
+		int num1 = 66;
+		
+		int num2 = 12;
+		
+		int feet = (int) (num1 / num2);
+		int inches = num1 % num2;
+		
+		System.out.println("The item is " + feet + "ft and " + inches + "in");
+		
+
+	}
+
+}
