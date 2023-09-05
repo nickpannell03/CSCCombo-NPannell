590837074b12b78973116b32ada27e22668c06ea
Pannell <S03012956@ccc.ccofc.edu>
2023-09-05 15:59:17 -0600

    Stringfun

---
+++ Stringfun/src/Stringfun.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Stringfun {
+
+	private static char[] answer;
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		String day =  "Tuesday";
+		
+		int date = 5;
+		
+		String month = "September";
+		
+		int year = 2023;
+		
+		System.out.println(day + " , " + month + " , " + date + " , " + year);
+		
+		System.out.print(day + " ");
+		System.out.print(month + " ");
+		System.out.print(date + " ");
+		System.out.println(year + " ");
+		
+		System.out.println("Do you know how to get input?");
+		answer = input. Nextline());
+		System.out.println(answer);
+
+	}
+
+}
