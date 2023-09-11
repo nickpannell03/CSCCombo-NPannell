08192c1ef9f0d058e1d150e785fc309f4c0caccb
Pannell <S03012956@ccc.ccofc.edu>
2023-09-11 15:57:59 -0600

    Tempconversion

---
+++ Tempconversion/src/Tempconversion.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tempconversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+Scanner input = new Scanner(System.in);
+		
+		String fahrenheit;
+		
+		System.out.println("Enter a temp in fahrenheit here: ");
+		
+		fahrenheit = input.next()
+		System.out.println("The temp goes here: " + fahrenheit);
+	}
+
+}
---
+++ Tempconversion/src/module-info.java
@@ -0,0 +1,2 @@
+module Tempconversion {
+}
