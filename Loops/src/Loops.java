d6281881dd2fdd1dec483d5c16ecb3ecbd644efb
Pannell <S03012956@ccc.ccofc.edu>
2023-09-14 15:59:34 -0600

    Loops

---
+++ Loops/src/Loops.java
@@ -0,0 +1,22 @@
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int count = 0;
+		int sum = 0;
+
+		while (count<10) {
+			sum += count;
+			count = count + 1;
+			//System.out.println("Count is " + count + " Sum is " + sum );
+			
+			
+			
+			}
+		for (count = 0; count<10; )
+	
+	}
+
+}
